8212f9ca201b00c7e52e7e82d5497c19e8a7c5c9
seeker53 <dishant.singh16@gmail.com>
2024-07-23 23:23:17 +0530

    mongodb connection

--- job-server/index.js
+++ job-server/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
+import { MongoClient, ServerApiVersion, ObjectId } from 'mongodb';
 dotenv.config({
     path : './.env'
 })
@@ -13,6 +14,32 @@ app.use(cors());    // next : add origin also
 app.use(express.json({limit : '16kb'}))
 app.use(express.urlencoded({extended:true, limit:'16kb'}))
 app.use(express.static("public"))
+
+const uri = process.env.MONGODB_URI;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
 
 
 app.get('/',(req,res)=>{
