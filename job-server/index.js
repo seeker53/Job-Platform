6f4ce3ac318e108e07438d9e694b4cbaaf29d62b
seeker53 <dishant.singh16@gmail.com>
2024-07-23 23:33:17 +0530

    crud functinality implementation

--- job-server/index.js
+++ job-server/index.js
@@ -31,6 +31,52 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    // create db
+    const db = client.db("JobPlatform");
+    const jobCollections = db.collection("jobs");
+
+    // post job
+    app.post("/post-job", async(req,res)=>{
+        const job = req.body;
+        job.createAt = new Date();
+        // console.log(job);
+        const result = await jobCollections.insertOne(job);
+        if(result?.insertedId){
+            return res.status(200).send(result);
+        }
+        else{
+            return res.status(504).send({
+                message:"couldn't upload job please try again",
+                status:false,
+            });
+        }
+        res.send(result);
+    })
+
+    //get all jobs
+    app.get("/all-jobs", async (req, res) => {
+        const result = await jobCollections.find({}).sort({createAt:-1}).toArray();
+        res.send(result);
+    })
+
+
+    //get single job by job id
+    app.get("/all-jobs/:id",async(req,res)=>{
+        const query = {_id : new ObjectId(req.params.id) || req.params.id};
+        const result = await jobCollections.findOne(query);
+        res.send(result);
+    })
+
+    //get all jobs by email
+    app.get("/all-jobs/:email",async(req,res)=>{
+        const query = {postedBy : req.params.email};
+        const result = await jobCollections.find(query).sort({createAt:-1}).toArray();
+        res.send(result);
+    })
+
+    //delete a job
+    app.delete("/job/:id")
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
